8a7b417a04def3e107f6c3aa70a3743218ff4e9a
boussida-feiza <158825953+boussida-feiza@users.noreply.github.com>
2024-03-30 23:52:59 +0100

    sstyle css

--- martex/src/pages/Login.js
+++ martex/src/pages/Login.js
@@ -1,34 +1,35 @@
 import loginImg from "../pages/image/login.jpg";
 import 'bootstrap/dist/css/bootstrap.css';
+import '../pages/Login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserLock} from '@fortawesome/free-solid-svg-icons'
 const Login =() => {
     return(
         <>
-<div className='container-fluid'>
-    <div className='row'>
+<div className='container-fluid  style'>
+    <div className='row '>
         <div className='col-6 lg-6 sm-3'> 
         <img src={loginImg} className='img-fluid' alt=''></img>
 
         </div>
-        <div className="col-6 lg-6 sm-3">
-            <form action="">
-                <h1>login to your account </h1>
-        <div class="mb-3 row" > 
+        <div className="col-6 lg-6 sm-3 d1">
+            <form action="" className="forme">
+                <h2 className="titel">Login to your account </h2>
+        <div class="mb-3 row F2" > 
         
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <label for="staticEmail" class="col-sm-2 col-form-label ">Email</label>
     <div class="col-sm-10">
       <input type="email"  class="form-control" id="" placeholder="email"/>
     </div>
   </div>
   <div class="mb-3 row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="" placeholder="password"/>
+      <input type="password" class="form-control " id="" placeholder="password"/>
     </div>
   </div>
-  <button className="btn btn-danger"> Login</button>
-  <FontAwesomeIcon icon={faUserLock} />
+  <button className="btn btn-primary b1"> Login</button> <br></br>
+  <FontAwesomeIcon icon={faUserLock } />
   <p>  creat new account
   </p>
   </form>
